194ef478d6cd59548820cbb52b498688ca162424
Thinh-Tan Thi <thinhprocf5879@gmail.com>
2023-11-25 14:55:35 +0700

    feat: property done: c-r

--- src/controllers/property.controller.js
+++ src/controllers/property.controller.js
@@ -1,14 +1,10 @@
 const mysql = require("mysql2");
-const mysql2 = require("mysql2/promise.js");
-const bcrypt = require("bcrypt");
 const config = require("../config/database.config.js");
 const responseMessage = require("../helpers/responseMessage.js");
-const generateToken = require("../helpers/authHelpers.js");
-const { connect } = require("../routes/user.router.js");
 
 const creatProperty = async (req, res) => {
   try {
-    const userID = req.body.userID;
+    const userID = req.user.data.userID;
     const {
       name,
       shortDescription,
@@ -22,15 +18,13 @@ const creatProperty = async (req, res) => {
       bathroom,
       constructionYear,
       parkingSlot,
-      postingDate,
-      expiryDate,
-      ppropertyCategory,
+      propertyCategory,
       category,
       area,
       imageUrls,
     } = req.body;
     const connection = mysql.createPool(config);
-    const sqlQuery = `CALL sp_insert_property(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    const sqlQuery = `CALL sp_insert_property(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
     connection.query(
       sqlQuery,
       [
@@ -47,9 +41,7 @@ const creatProperty = async (req, res) => {
         bathroom,
         constructionYear,
         parkingSlot,
-        postingDate,
-        expiryDate,
-        ppropertyCategory,
+        propertyCategory,
         category,
         area,
       ],
@@ -59,8 +51,10 @@ const creatProperty = async (req, res) => {
             .status(400)
             .json(responseMessage(err.message, null, "fail", null));
         }
-        const property_id = result.flat().property_id;
-
+        const data = result[0].map((item) => item.property_id);
+        console.log(data);
+        const property_id = data.flat();
+        console.log(property_id);
         const insertImagesQuery = `CALL sp_insert_property_images(?, ?)`;
 
         connection.query(
@@ -87,4 +81,273 @@ const creatProperty = async (req, res) => {
   }
 };
 
-module.exports = { creatProperty };
+const getListProperty = async (req, res) => {
+  try {
+    const pageNumber = req.query.pageNumber;
+    const pageSize = 16;
+
+    const connection = mysql.createPool(config);
+    const sqlQuery = `CALL sp_get_list_property(?, ?)`;
+    connection.query(sqlQuery, [pageNumber, pageSize], (err, result) => {
+      if (err) {
+        return res
+          .status(400)
+          .json(responseMessage(err.message, null, "fail", null));
+      }
+      const properties = result[0].flat();
+      if (!properties || properties.length == 0) {
+        // Property not found
+        return res
+          .status(404)
+          .json(responseMessage("Property not found", null, "fail", null));
+      }
+      return res
+        .status(200)
+        .json(responseMessage("Property found", properties, "success", null));
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .json(responseMessage("Internal Server error", null, "fail", null));
+  }
+};
+
+const getUserInterest = async (req, res) => {
+  try {
+    const userId = req.user.data.userID;
+    const connection = mysql.createPool(config);
+    const sqlQuery = `CALL sp_get_user_interest(?)`;
+    connection.query(sqlQuery, [userId], function handleQuery(err, result) {
+      if (err) {
+        return res
+          .status(400)
+          .json(responseMessage(err.sqlMessage, null, "fail", null));
+      }
+      const userData = result[0].flat();
+      if (!userData || userData.length == 0) {
+        // User not found
+        return res
+          .status(404)
+          .json(responseMessage("List not found", null, "fail", null));
+      }
+      return res
+        .status(200)
+        .json(responseMessage("User interest list", userData, "success", null));
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .json(responseMessage("Internal Server error", null, "fail", null));
+  }
+};
+
+const updateProperty = async (req, res) => {
+  try {
+    const userId = req.user.data.userID;
+    const {
+      propertyId,
+      name,
+      shortDescription,
+      detailDescription,
+      price,
+      provinceCode,
+      districtCode,
+      wardCode,
+      address,
+      bedroom,
+      bathroom,
+      constructionYear,
+      parkingSlot,
+      propertyCategory,
+      category,
+      area,
+      imageUrls,
+    } = req.body;
+    const connection = mysql.createPool(config);
+    const sqlQuery = `CALL sp_update_property(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    connection.query(
+      sqlQuery,
+      [
+        userId,
+        propertyId,
+        name,
+        shortDescription,
+        detailDescription,
+        price,
+        provinceCode,
+        districtCode,
+        wardCode,
+        address,
+        bedroom,
+        bathroom,
+        constructionYear,
+        parkingSlot,
+        propertyCategory,
+        category,
+        area,
+      ],
+      (err, result) => {
+        if (err) {
+          return res
+            .status(400)
+            .json(responseMessage(err.message, null, "fail", null));
+        }
+
+        const imageUrlsQuery = `CALL sp_update_property_images(?, ?)`;
+      }
+    );
+  } catch (err) {}
+};
+
+const getListPropertyByUser = async (req, res) => {
+  try {
+    const userId = req.user.data.userID;
+    const connection = mysql.createPool(config);
+    const sqlQuery = `CALL sp_get_list_property_by_user(?)`;
+    connection.query(sqlQuery, [userId], function handleQuery(err, result) {
+      if (err) {
+        return res
+          .status(400)
+          .json(responseMessage(err.sqlMessage, null, "fail", null));
+      }
+      const userData = result[0].flat();
+      if (!userData || userData.length == 0) {
+        // User not found
+        return res
+          .status(404)
+          .json(responseMessage("List not found", null, "fail", null));
+      }
+      return res
+        .status(200)
+        .json(responseMessage("User interest list", userData, "success", null));
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .json(responseMessage("Internal Server error", null, "fail", null));
+  }
+};
+
+const getDetailProperty = async (req, res) => {
+  try {
+    const propertyId = req.params.propertyId;
+    const connection = mysql.createPool(config);
+    const sqlQuery = `CALL sp_get_detail_property(?)`;
+    connection.query(sqlQuery, [propertyId], function handleQuery(err, result) {
+      if (err) {
+        return res
+          .status(400)
+          .json(responseMessage(err.sqlMessage, null, "fail", null));
+      }
+      const userData = result[0].flat();
+      if (!userData || userData.length == 0) {
+        // User not found
+        return res
+          .status(404)
+          .json(responseMessage("Property not found", null, "fail", null));
+      }
+      return res
+        .status(200)
+        .json(responseMessage("Property detail", userData, "success", null));
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .json(responseMessage("Internal Server error", null, "fail", null));
+  }
+};
+
+const getProvinces = async (req, res) => {
+  try {
+    const connection = mysql.createPool(config);
+    const sqlQuery = `CALL sp_get_province_list()`;
+    connection.query(sqlQuery, [], function handleQuery(err, result) {
+      if (err) {
+        return res
+          .status(400)
+          .json(responseMessage(err.sqlMessage, null, "fail", null));
+      }
+      const userData = result[0].flat();
+      if (!userData || userData.length == 0) {
+        // User not found
+        return res
+          .status(404)
+          .json(responseMessage("Provinces not found", null, "fail", null));
+      }
+      return res
+        .status(200)
+        .json(responseMessage("Provinces", userData, "success", null));
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .json(responseMessage("Internal Server error", null, "fail", null));
+  }
+};
+
+const getDistricts = async (req, res) => {
+  try {
+    const provinceCode = req.params.provinceCode;
+    const connection = mysql.createPool(config);
+    const sqlQuery = `CALL sp_get_district_list(?)`;
+    connection.query(
+      sqlQuery,
+      [provinceCode],
+      function handleQuery(err, result) {
+        if (err) {
+          return res
+            .status(400)
+            .json(responseMessage(err.sqlMessage, null, "fail", null));
+        }
+        const userData = result[0].flat();
+        if (!userData || userData.length == 0) {
+          // User not found
+          return res
+            .status(404)
+            .json(responseMessage("Districts not found", null, "fail", null));
+        }
+        return res
+          .status(200)
+          .json(responseMessage("Districts", userData, "success", null));
+      }
+    );
+  } catch (err) {
+    return res
+      .status(500)
+      .json(responseMessage("Internal Server error", null, "fail", null));
+  }
+};
+
+const getWards = async (req, res) => {
+  const districtCode = req.params.districtCode;
+  const connection = mysql.createPool(config);
+  const sqlQuery = `CALL sp_get_ward_list(?)`;
+  connection.query(sqlQuery, [districtCode], function handleQuery(err, result) {
+    if (err) {
+      return res
+        .status(400)
+        .json(responseMessage(err.sqlMessage, null, "fail", null));
+    }
+    const userData = result[0].flat();
+    if (!userData || userData.length == 0) {
+      // User not found
+      return res
+        .status(404)
+        .json(responseMessage("Wards not found", null, "fail", null));
+    }
+    return res
+      .status(200)
+      .json(responseMessage("Wards", userData, "success", null));
+  });
+};
+
+module.exports = {
+  creatProperty,
+  getListProperty,
+  getUserInterest,
+  getDetailProperty,
+  getListPropertyByUser,
+  getProvinces,
+  getDistricts,
+  getWards,
+};
--- src/routes/property.router.js
+++ src/routes/property.router.js
@@ -1,11 +1,30 @@
-const { creatProperty } = require("../controllers/property.controller.js");
+const {
+  creatProperty,
+  getListProperty,
+  getUserInterest,
+  getDetailProperty,
+  getListPropertyByUser,
+  getProvinces,
+  getDistricts,
+  getWards,
+} = require("../controllers/property.controller.js");
 const express = require("express");
 const authenticateJWT = require("../middlewares/authMiddleware.js");
 
 const router = express.Router();
 
 router.route("/createproperty").post(authenticateJWT, creatProperty);
-// router.route("/checkUser/:email").get(checkUser);
+router.route("/getlistproperty").get(getListProperty);
+router.route("/getuserinterest").get(authenticateJWT, getUserInterest);
+router
+  .route("/getdetailproperty/:propertyId")
+  .get(authenticateJWT, getDetailProperty);
+router
+  .route("/getlistpropertybyuser")
+  .get(authenticateJWT, getListPropertyByUser);
+router.route("/getprovinces").get(getProvinces);
+router.route("/getdistricts/:provinceCode").get(getDistricts);
+router.route("/getwards/:districtCode").get(getWards);
 // router.route("/login").post(loginUser);
 // router.route("/logout").post(authenticateJWT, logoutUser);
 // router.route("/getUserInfor").get(authenticateJWT, getUserInfor);
