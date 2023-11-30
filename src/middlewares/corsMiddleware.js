f4e556513b1e1fe31886fc78d318d8b9f75583ee
Thinh-Tan Thi <thinhprocf5879@gmail.com>
2023-12-01 01:26:44 +0700

    update: cors

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: "http://localhost:3000", // Replace with your frontend domain
+  origin: ["http://localhost:3000", "https://thedreamhome.click"], // Replace with your frontend domain
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: false, // Enable credentials (cookies, authorization headers, etc.)
   optionsSuccessStatus: 200, // Respond with 204 (No Content) for preflight requests
